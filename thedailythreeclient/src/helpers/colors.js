3389be7e6d277b72cb5e0d75924ae4871e4f3100
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-05-21 14:10:15 -0500

    added chart.js and doughnut chart to moodtracker view - got it to work finally

---
+++ thedailythreeclient/src/helpers/colors.js
@@ -0,0 +1,9 @@
+const colors = [
+  'rgba(231, 72, 86, 91)',
+  'rgba(0, 153, 188, 74)',
+  'rgba(247, 99, 12, 97)',
+  'rgba(234, 0, 94, 92)',
+  'rgba(177, 70, 194, 76)',
+];
+
+export default colors;

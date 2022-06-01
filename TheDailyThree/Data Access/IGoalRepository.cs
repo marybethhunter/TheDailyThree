555b2c3b9524acd52771326cf867bf3b86f64399
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-05-31 20:07:39 -0500

    created model, table in DB, controller, and repositories for goals

---
+++ TheDailyThree/Data Access/IGoalRepository.cs
@@ -0,0 +1,14 @@
+﻿using TheDailyThree.Models;
+
+namespace TheDailyThree.Data_Access
+{
+    public interface IGoalRepository
+    {
+        List<Goal> GetAllUserGoals(int userId);
+        List<Goal> GetUserGoalsByUid(string uid);
+        Goal GetGoalById(int id);
+        void AddGoal(Goal newGoal);
+        void DeleteGoal(int id);
+        public void UpdateGoal(Goal goalToUpdate);
+    }
+}
---
+++ TheDailyThree/SQL/GoalsInit.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Goals (
+    Id INTEGER IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+    Title VARCHAR(255) NOT NULL,
+    [Description] VARCHAR(1000) NOT NULL,
+    Completed BIT NOT NULL,
+    UserId INTEGER NOT NULL,
+    CONSTRAINT [FK_Goal_User] FOREIGN KEY (UserId) REFERENCES [User](Id),
+);
+
+--DUMMY DATA FOR THIS TABLE AT THE START
+INSERT INTO Goals (Title, [Description], Completed, UserId) VALUES ('Water', 'Start drinking 8 glasses of water every day!', 0, 1);

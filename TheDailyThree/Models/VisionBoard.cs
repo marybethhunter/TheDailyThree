ae258c41502d824d9000128e84cc7b503b125c77
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-06-04 10:44:11 -0500

    created models for visionboards and visionboard images, made controller and repositories for visionboards

---
+++ TheDailyThree/Models/VisionBoard.cs
@@ -0,0 +1,9 @@
+﻿namespace TheDailyThree.Models
+{
+    public class VisionBoard
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int UserId { get; set; }
+    }
+}

b730ee0c42db8507789e440a8d2d1a05ead54ca9
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-06-05 14:59:32 -0500

    added visionboardimagecontroller + methods + added to program.cs

--- TheDailyThree/Controllers/VisionBoardImageController.cs
+++ TheDailyThree/Controllers/VisionBoardImageController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TheDailyThree.Data_Access;
+using TheDailyThree.Models;
 
 namespace TheDailyThree.Controllers
 {
@@ -7,5 +9,68 @@ namespace TheDailyThree.Controllers
     [ApiController]
     public class VisionBoardImageController : ControllerBase
     {
+        private readonly IVisionBoardImageRepository _visionBoardImageRepo;
+        public VisionBoardImageController(IVisionBoardImageRepository visionBoardImageRepo)
+        {
+            _visionBoardImageRepo = visionBoardImageRepo;
+        }
+
+        [HttpGet("{vbId}")]
+        public List<VisionBoardImage> GetAllVisionBoardImagesByVisionBoardId(int vbId)
+        {
+            return _visionBoardImageRepo.GetAllVisionBoardImagesByVBId(vbId);
+        }
+
+        [HttpGet("id/{id}")]
+        public IActionResult GetSingleVisionBoardImage(int id)
+        {
+            var match = _visionBoardImageRepo.GetVisionBoardImageById(id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+            return Ok(match);
+        }
+
+        [HttpPost]
+        public IActionResult CreateVisionBoardImage(VisionBoardImage newVisionBoardImage)
+        {
+            if (newVisionBoardImage == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _visionBoardImageRepo.AddVisionBoardImage(newVisionBoardImage);
+                return Ok(newVisionBoardImage);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var match = _visionBoardImageRepo.GetVisionBoardImageById(id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _visionBoardImageRepo.DeleteVisionBoardImage(id);
+                return NoContent();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(VisionBoardImage visionBoardImageToUpdate)
+        {
+            var match = _visionBoardImageRepo.GetVisionBoardImageById(visionBoardImageToUpdate.Id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+            _visionBoardImageRepo.UpdateVisionBoardImage(visionBoardImageToUpdate);
+            return NoContent();
+        }
     }
 }
